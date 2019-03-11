5db77b2b2566412a681a66f888163cb68888298c
graphicskart <graphicskart2018@gmail.com>
2019-03-11 21:12:37 +0530

    Updates

---
+++ precache-manifest.ab5311a80605ee37f97ad4fe82166c97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4030701844ea01feb1d7",
+    "url": "/bootstrap-alerts/static/js/runtime~main.604e339a.js"
+  },
+  {
+    "revision": "2b27cb543d4331d83dd3",
+    "url": "/bootstrap-alerts/static/js/main.e2f6f18c.chunk.js"
+  },
+  {
+    "revision": "69805f2c4dc7179e2ebb",
+    "url": "/bootstrap-alerts/static/js/2.0b8b0281.chunk.js"
+  },
+  {
+    "revision": "2b27cb543d4331d83dd3",
+    "url": "/bootstrap-alerts/static/css/main.4c7f5a04.chunk.css"
+  },
+  {
+    "revision": "901ef3931197121527284c26362624cf",
+    "url": "/bootstrap-alerts/index.html"
+  }
+];
